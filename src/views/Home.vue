7797f20e3c02234cd1edd3fce6bf61f5f606ebf5
Vinicius-Muller <viniciusmul98@gmail.com>
2020-12-14 19:54:23 -0300

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="title-box">
+    <img class="logo" src="@/assets/Logo.svg" alt="Happy">
+    <h1>Leve a felicidade para o mundo</h1>
+    </div>
+    <img class="landing" src="@/assets/landing.svg" alt="Leve a felicidade para o mundo">
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+
+<style scoped>
+
+.home {
+  min-height: 100vh;
+  width: 100vw;
+  background-color: rgb(157, 184, 235);
+  display:flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 40px;
+}
+
+.home .title-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 40%;
+  height: 50vh;
+}
+
+.home .title-box .logo {
+  width: 300px;
+}
+
+.home .title-box h1 {
+  color: white;
+  font-size:2.5rem;
+}
+
+.home .title-box p {
+  color: white;
+  font-size: 1.2rem;
+  text-align: center;
+}
+
+.home .landing {
+  width: 40%;
+  height: 80vh;
+}
+
+</style>
