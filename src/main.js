261026d51e034a3643e621b9bd00d5ce58e150ba
Vinicius-Muller <viniciusmul98@gmail.com>
2020-12-15 16:18:12 -0300

    orphanages locations listed

--- src/main.js
+++ src/main.js
@@ -6,14 +6,6 @@ import store from './store'
 
 Vue.config.productionTip = false
 
-import * as VueGoogleMaps from 'vue2-google-maps'
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyAQ0-bGccVRYbBWtD_Sq6gtPcycFhBv5jE',
-    libraries: 'places',
-  },
-})
-
 new Vue({
   router,
   store,
--- src/views/Orphanages.vue
+++ src/views/Orphanages.vue
@@ -1,23 +1,12 @@
 <template>
   <div class="wrapper">
     <section class="side-bar">
+      <router-link to="/">
       <img class="logo" src="@/assets/Logo.svg" alt="Happy">
+      </router-link>
+      <p>Ajude a transformar o mundo em um lugar melhor.</p>
     </section>
-    <GmapMap
-  :center="{lat:-29.9234021, lng:-51.4119127}"
-  :zoom="8"
-  map-type-id="terrain"
-  style="width: 70%; height: 100vh"
->
-  <GmapMarker
-    :key="index"
-    v-for="(m, index) in markers"
-    :position="m.position"
-    :clickable="true"
-    :draggable="true"
-    @click="center=m.position"
-  />
-</GmapMap>
+    <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d887820.4050671754!2d-51.58234180067377!3d-29.631335827372475!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sorfanatos!5e0!3m2!1spt-BR!2sbr!4v1608058474340!5m2!1spt-BR!2sbr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
   </div>
 </template>
 
@@ -40,8 +29,25 @@ export default {
   height: 100vh;
   background-color: rgb(157, 184, 235);
   display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 50px 0;
+}
+
+.side-bar a {
+  display: flex;
   justify-content: center;
-  align-items: flex-start;
+}
+
+.side-bar p {
+  color: white;
+  text-align: center;
+  font-size: 1.3rem;
+}
+
+.map {
+  width: 70%;
+  height: 100vh;
 }
 
 </style>
