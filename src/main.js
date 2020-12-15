22a2555c3544de6bc78cbded2b17b00e0514b7ea
Vinicius-Muller <viniciusmul98@gmail.com>
2020-12-15 11:58:57 -0300

    google maps implemented

--- src/main.js
+++ src/main.js
@@ -3,7 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
 Vue.config.productionTip = false
+
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyAQ0-bGccVRYbBWtD_Sq6gtPcycFhBv5jE',
+    libraries: 'places',
+  },
+})
 
 new Vue({
   router,
--- src/views/Orphanages.vue
+++ src/views/Orphanages.vue
@@ -1,7 +1,24 @@
 <template>
-  <section>
-    <p>orphanages</p>
-  </section>
+  <div class="wrapper">
+    <section class="side-bar">
+      <img class="logo" src="@/assets/Logo.svg" alt="Happy">
+    </section>
+    <GmapMap
+  :center="{lat:-29.9234021, lng:-51.4119127}"
+  :zoom="8"
+  map-type-id="terrain"
+  style="width: 70%; height: 100vh"
+>
+  <GmapMarker
+    :key="index"
+    v-for="(m, index) in markers"
+    :position="m.position"
+    :clickable="true"
+    :draggable="true"
+    @click="center=m.position"
+  />
+</GmapMap>
+  </div>
 </template>
 
 <script>
@@ -10,6 +27,21 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+.wrapper {
+  width:100vw;
+  min-height: 100vh;
+  display:flex;
+}
+
+.side-bar {
+  width: 30%;
+  height: 100vh;
+  background-color: rgb(157, 184, 235);
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+}
 
 </style>
